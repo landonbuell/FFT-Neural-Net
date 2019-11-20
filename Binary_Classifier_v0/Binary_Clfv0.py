8363255f0d854c3574f2e7d72b8dff366ac66255
landonbuell <lhb1007@wildcats.unh.edu>
2019-11-20 10:13:46 -0500

    20 November 2019
    - Begun Outline for Binary Classifier Program

--- Binary_Classifier_v0/Binary_Clfv0.py
+++ Binary_Classifier_v0/Binary_Clfv0.py
@@ -5,11 +5,15 @@ Binary Classifier - v0
 5 Novemeber 2019
 """
 
+            #### IMPORTS ####
+
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
 import sklearn.model_selection as model
 from sklearn.linear_model import SGDClassifier
+
+
 
 
             #### CLASS OBJECTS ####
--- Binary_Classifier_v0/Binary_Clfv0_main.py
+++ Binary_Classifier_v0/Binary_Clfv0_main.py
@@ -5,6 +5,8 @@ Binary Classifier - v0
 5 Novemeber 2019
 """
 
+            #### IMPORTS ####
+
 import numpy as np
 import matplotlib.pyplot as plt
 import Binary_Clfv0 as Clfv0
@@ -13,5 +15,14 @@ import Binary_Clfv0 as Clfv0
 
 if __name__ == '__main__':
 
+    """ Program Outline:
+    ----------------
+    - Collect All Frequency Spectra data as an N x M array     
+            Each row is the spectrum for a particular audio file
+            Put name of file into array of names
+            T/F array indicating if strings or not
+    - Feed
+    """
 
     pass
+     
