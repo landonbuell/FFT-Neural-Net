3b233722ecd746797b1918f93816456a46fb039d
landonbuell <lhb1007@wildcats.unh.edu>
2019-11-24 20:22:31 -0500

    24 November 2019
    - Created Classifier v1 Folder
    - Test program to classify string vs. not string by frequnecy spectra

--- Binary_Classifier_v0/Binary_Clfv0.py
+++ Binary_Classifier_v0/Binary_Clfv0.py
@@ -9,13 +9,42 @@ Binary Classifier - v0
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 import sklearn.metrics as metrics
 import sklearn.model_selection as model
 from sklearn.linear_model import SGDClassifier
 
-
-
-
             #### CLASS OBJECTS ####
 
+class file_obj ():
+    """ Create Raw wavefile object """
+
+    def __init__(self,root,file):
+        """ Initialize Class Object """
+        self.dirpath = root                     # intial storage path
+        self.file = file                        # filename
+
             #### FUNCTIONS DEFINTIONS ####
+
+def read_directory(dir):
+    """
+    Read all files in given directory path
+    --------------------------------
+    dir (str) : desired directory path
+    --------------------------------
+    returns list of all '.txt' files
+    """
+    file_objs = []                          # list to '.txt' hold files objs
+    for roots,dirs,files in os.walk(dir):   # all objects in the path
+        for file in files:                  # files in list of files
+            if file.endswith('.txt'):       # if '.txt' file
+                wav = file_obj(roots,file)  # make instance
+                file_objs.append(wav)       # add to list of file objs
+    return file_objs                        # return lists  of files
+
+def Read_CSV(filename,cols=[0,1,2,3]):
+    """ Read Data From CSV File into pandas dataframe """
+    data = np.loadtxt(filename,dtype=float,delimiter='\t',
+                      skiprows=1,usecols=cols)
+    return data
--- Binary_Classifier_v0/Binary_Clfv0_main.py
+++ Binary_Classifier_v0/Binary_Clfv0_main.py
@@ -9,20 +9,48 @@ Binary Classifier - v0
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 import Binary_Clfv0 as Clfv0
+
 
             #### MAIN EXECUTABLE FUNCTION ####
 
 if __name__ == '__main__':
 
-    """ Program Outline:
+    """
+    Program Outline:
     ----------------
     - Collect All Frequency Spectra data as an N x M array     
             Each row is the spectrum for a particular audio file
             Put name of file into array of names
             T/F array indicating if strings or not
-    - Feed
+    - Feed data into SGD Classifier
     """
 
-    pass
+    FFT_dir = 'C:/Users/Landon/Documents/wav_FFTs'
+
+
+    os.chdir(FFT_dir)
+    files = Clfv0.read_directory(FFT_dir)
+    print("Number of Files:",len(files))
+    print("Collecting Data...")
+    L_FFT_Frame = pd.DataFrame()
+
+    """ Classifying By Frequency Spectra """
+
+    for file in files:                              # It. through list of objs 
+        os.chdir(file.dirpath)                      # move to directory path
+        frame = Clfv0.Read_CSV(file.file,cols=[2])  # read the CSV file
+        L_FFT_Frame[str(file.file)] = frame         # add to large data frame
+
+            #### We Now Have all files sotred into a DataFrame #### 
+
+        FFT_Training,FFT_Testing = Clfv0.model.train_test_split(L_FFT_Frame,
+                                                          test_size=0.1,random_state=42)
+            
+        
+
+
+    
      
